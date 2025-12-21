67caafdcd36da38ca0e9a293cd4a43293926a5b6
Vaibhav Kaushal <dunkinferocrow2711@gmail.com>
2025-12-22 00:29:19 +0530

    display height and weight

--- app/details.tsx
+++ app/details.tsx
@@ -5,12 +5,17 @@ import { useLocalSearchParams } from "expo-router";
 import { Stack } from "expo-router";
 
 export default function Details() {
+ type PokemonDetails = {
+  height: number;
+  weight: number;
+};
   const params = useLocalSearchParams();
   // console.log("Params received:", params);
   // console.log(typeof params.name);
   const name = params.name.toString().charAt(0).toUpperCase() + params.name.slice(1) //this to capitalise the first word and use that
   // console.log("Name:", name)
-  const [pokeDetails, setPokeDetails] = useState({}); // do i need to write null or i can leave () as it is?? => Ans) () is undefined, whereas null means clearly we have nothing yet,,, If data comes later → start with null
+  const [pokeDetails, setPokeDetails] = useState<PokemonDetails | null>(null); // do i need to write null or i can leave () as it is?? => Ans) () is undefined, whereas null means clearly we have nothing yet,,, If data comes later → start with null
+
 
   useEffect(()=>{
     if (params.name) {
@@ -43,7 +48,12 @@ export default function Details() {
       <Stack.Screen options={{ title: params.name as string }} />
       <ScrollView contentContainerStyle={{ gap: 16, padding: 16, justifyContent:"center", alignItems:"center" }}>
         <Text style={styles.text}>Details of {name}</Text>
-
+        {pokeDetails && (
+          <>
+            <Text> Height of this {params.name} is: {pokeDetails.height}  </Text>
+            <Text> Weight of this {params.name} is: {pokeDetails.weight}  </Text>
+          </>
+        )}
       </ScrollView>
     </>
   );
